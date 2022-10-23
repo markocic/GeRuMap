9dead70a51748bbdab4fec438da7fa352cabdf27
Marko Kocic <markokocic2000@gmail.com>
2022-10-23 20:09:00 +0200

    dodati new project i exit u menubar kao i nove MINIMALISTICKE CUSTOM MADE ikonice

--- src/raf/dsw/gerumap/gui/swing/controller/NewProjectAction.java
+++ src/raf/dsw/gerumap/gui/swing/controller/NewProjectAction.java
@@ -1,4 +1,20 @@
 package raf.dsw.gerumap.gui.swing.controller;
 
-public class NewProjectAction {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+public class NewProjectAction extends AbstractGerumapAction {
+    public NewProjectAction() {
+        putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(
+                KeyEvent.VK_F4, ActionEvent.ALT_MASK));
+        putValue(SMALL_ICON, loadIcon("images/plus_icon.png"));
+        putValue(NAME, "New Project");
+        putValue(SHORT_DESCRIPTION, "New Project");
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+
+    }
+
 }
--- src/raf/dsw/gerumap/gui/swing/view/MyMenuBar.java
+++ src/raf/dsw/gerumap/gui/swing/view/MyMenuBar.java
@@ -1,16 +1,25 @@
 package raf.dsw.gerumap.gui.swing.view;
 
+import raf.dsw.gerumap.gui.swing.controller.ExitAction;
+
 import javax.swing.*;
+
 import java.awt.*;
 
 public class MyMenuBar extends JMenuBar {
     public MyMenuBar() {
         init();
     }
-    JMenuItem file = new JMenuItem("File");
+    JMenu file = new JMenu("File");
+
+    JMenuItem newProject = new JMenuItem("New Project", new ImageIcon("../controller/images/plus_icon.png"));
+    JMenuItem exit = new JMenuItem(new ExitAction());
+
 
     private void init() {
         this.add(file);
+        file.add(newProject);
+        file.add(exit);
     }
 
 
