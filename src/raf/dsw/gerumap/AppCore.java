a814139763e893ba824f6ce80d95a4317dcde03e
Danilo149 <danilo.g.joncic2@gmail.com>
2022-10-24 00:02:41 +0200

    Toolbar gotov koristeci abstractGeruMap potomke

--- src/raf/dsw/gerumap/AppCore.java
+++ src/raf/dsw/gerumap/AppCore.java
@@ -1,10 +1,11 @@
 package raf.dsw.gerumap;
 
+import raf.dsw.gerumap.core.ApplicationFramework;
 import raf.dsw.gerumap.gui.swing.view.MainFrame;
 
 
 
-public class AppCore {
+public class AppCore extends ApplicationFramework {
     public static void main(String[] args) {
         MainFrame.getInstance();
 
--- src/raf/dsw/gerumap/gui/swing/view/Toolbar.java
+++ src/raf/dsw/gerumap/gui/swing/view/Toolbar.java
@@ -1,11 +1,17 @@
 package raf.dsw.gerumap.gui.swing.view;
 
+import raf.dsw.gerumap.gui.swing.controller.ExitAction;
+import raf.dsw.gerumap.gui.swing.controller.NewProjectAction;
+import raf.dsw.gerumap.gui.swing.controller.NewSomethingAction;
+import raf.dsw.gerumap.gui.swing.controller.QuitAction;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Toolbar extends JToolBar {
 
-
+    JButton tbIzadji;
+    JButton tbNovi;
 
     public Toolbar() {
         init();
@@ -13,6 +19,10 @@ public class Toolbar extends JToolBar {
 
 
     private void init() {
+    tbIzadji = new JButton(new QuitAction());
+    tbNovi = new JButton(new NewSomethingAction());
+    this.add(tbIzadji);
+    this.add(tbNovi);
 
 
 
