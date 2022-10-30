7371b8457f20db004f45584d93b68daca8e42b08
markocic <markokocic2000@gmail.com>
2022-10-30 20:52:16 +0100

    mapnode implementiran

--- src/raf/dsw/gerumap/gui/swing/model/MapNode.java
+++ src/raf/dsw/gerumap/gui/swing/model/MapNode.java
@@ -1,4 +1,38 @@
 package raf.dsw.gerumap.gui.swing.model;
 
-public interface MapNode {
+public abstract class MapNode {
+
+    private String name;
+    private MapNode parent;
+
+    public MapNode(String name, MapNode parent) {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null && obj instanceof MapNode)
+        {
+            MapNode node = (MapNode) obj;
+            return this.getName().equals(node.getName());
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MapNode getParent() {
+        return parent;
+    }
+
+    public void setParent(MapNode parent) {
+        this.parent = parent;
+    }
 }
