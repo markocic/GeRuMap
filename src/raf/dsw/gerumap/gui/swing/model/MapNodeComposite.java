add87e7fefad2e33f8caed15131684294997b6cc
markocic <markokocic2000@gmail.com>
2022-10-30 20:56:12 +0100

    dodata implementacija za MapNodeComposite

--- src/raf/dsw/gerumap/gui/swing/model/MapNodeComposite.java
+++ src/raf/dsw/gerumap/gui/swing/model/MapNodeComposite.java
@@ -1,4 +1,28 @@
 package raf.dsw.gerumap.gui.swing.model;
 
-public interface MapNodeComposite {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class MapNodeComposite extends MapNode{
+
+    List<MapNode> children;
+
+    public MapNodeComposite(String name, MapNode parent) {
+        super(name, parent);
+        this.children = new ArrayList<>();
+    }
+
+    public MapNodeComposite(String name, MapNode parent, List<MapNode> children) {
+        super(name, parent);
+        this.children = children;
+    }
+
+    public abstract void addChild(MapNode child);
+
+    public MapNode getChildByName(String name) {
+        for (MapNode child: children) {
+            if (name.equals(child.getName())) return child;
+        }
+        return null;
+    }
 }
