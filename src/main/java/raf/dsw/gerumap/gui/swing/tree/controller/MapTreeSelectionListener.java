cb955183813d7c02a134945ef66d063d12a1bb30
Danilo149 <danilo.g.joncic2@gmail.com>
2022-11-02 16:53:10 +0100

    TreeSelectionListener je uradjen!

--- src/main/java/raf/dsw/gerumap/gui/swing/tree/controller/MapTreeSelectionListener.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/tree/controller/MapTreeSelectionListener.java
@@ -1,4 +1,17 @@
 package raf.dsw.gerumap.gui.swing.tree.controller;
 
-public class MapTreeSelectionListener {
+import raf.dsw.gerumap.gui.swing.tree.model.MapTreeItem;
+
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.TreePath;
+
+public class MapTreeSelectionListener implements TreeSelectionListener {
+    @Override
+    public void valueChanged(TreeSelectionEvent e) {
+        TreePath put = e.getPath();
+        MapTreeItem SelektovaniTreeItem = (MapTreeItem) put.getLastPathComponent();
+        System.out.println("Izabrani cvor:" + SelektovaniTreeItem.getMapNode().getName());
+        System.out.println("getPath: " + e.getPath());
+    }
 }
