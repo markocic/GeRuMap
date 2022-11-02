6e7fec4900e8b1ce0dd3e5144a452d1ea4419466
markocic <markokocic2000@gmail.com>
2022-11-02 15:47:46 +0100

    dodata polja za autora i path u klasi project

--- src/main/java/raf/dsw/gerumap/repository/implementation/Project.java
+++ src/main/java/raf/dsw/gerumap/repository/implementation/Project.java
@@ -3,8 +3,31 @@ package raf.dsw.gerumap.repository.implementation;
 import raf.dsw.gerumap.repository.composite.MapNode;
 
 public class Project extends MapNode {
+    private String author;
+    private String path;
     public Project(String name, MapNode parent) {
         super(name, parent);
     }
 
+    public Project(String name, MapNode parent, String author, String path) {
+        super(name, parent);
+        this.author = author;
+        this.path = path;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
