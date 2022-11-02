2ad84e413eed087661642f53db43b937dc084d0e
Danilo149 <danilo.g.joncic2@gmail.com>
2022-11-02 16:48:50 +0100

    TreeCellEditor je uradjen!

--- src/main/java/raf/dsw/gerumap/gui/swing/tree/controller/MapTreeCellEditor.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/tree/controller/MapTreeCellEditor.java
@@ -1,4 +1,49 @@
 package raf.dsw.gerumap.gui.swing.tree.controller;
 
-public class MapTreeCellEditor {
+import raf.dsw.gerumap.gui.swing.tree.model.MapTreeItem;
+import raf.dsw.gerumap.gui.swing.tree.view.MapTreeCellRenderer;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeCellEditor;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
+
+public class MapTreeCellEditor extends DefaultTreeCellEditor implements ActionListener {
+
+    private Object clickedOn = null;
+    private JTextField edit = null;
+    public MapTreeCellEditor(JTree tree, DefaultTreeCellRenderer renderer) {
+        super(tree, renderer);
+    }
+
+    public Component getTreeCellEditorComponent(JTree tree, Object obj, boolean argument,
+                                                boolean argument2, boolean argument3, int broj){
+        clickedOn = obj;
+        edit = new JTextField(obj.toString());
+        edit.addActionListener(this);
+        return edit;
+    }
+
+
+
+    public boolean isCellEditable(EventObject arguemnt){
+        if(arguemnt instanceof MouseEvent)
+            if(((MouseEvent) arguemnt).getClickCount() == 3){ //caka da nakon 3 klika imamo promijenu
+                return true;
+            }
+
+        return false;
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(!(clickedOn instanceof MapTreeCellRenderer))
+            return;
+        MapTreeItem clicked = (MapTreeItem) clickedOn;
+        clicked.setName(e.getActionCommand());
+    }
+
 }
