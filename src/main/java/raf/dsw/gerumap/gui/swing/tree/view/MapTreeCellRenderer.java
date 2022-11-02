4cb4be9de75646b85b4627c33d82fb84f115671a
Danilo149 <danilo.g.joncic2@gmail.com>
2022-11-02 16:27:49 +0100

    MapTreeCellRenderer je uradjen

--- src/main/java/raf/dsw/gerumap/gui/swing/tree/view/MapTreeCellRenderer.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/tree/view/MapTreeCellRenderer.java
@@ -1,4 +1,37 @@
 package raf.dsw.gerumap.gui.swing.tree.view;
 
-public class MapTreeCellRenderer {
+import raf.dsw.gerumap.gui.swing.tree.model.MapTreeItem;
+import raf.dsw.gerumap.repository.implementation.Project;
+import raf.dsw.gerumap.repository.implementation.ProjectExplorer;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import java.awt.*;
+import java.net.URL;
+
+public class MapTreeCellRenderer extends DefaultTreeCellRenderer {
+
+    public MapTreeCellRenderer() {
+    }
+
+    public Component getTreeCellRendererComponent(JTree tree, Object val, boolean sel, boolean expanded,
+                                                  boolean list, int row, boolean hasfocus){
+        super.getTreeCellRendererComponent(tree,val,sel,expanded,list,row,hasfocus);
+        URL imageURL = null;
+
+        if(((MapTreeItem)val).getMapNode() instanceof ProjectExplorer){
+            imageURL = getClass().getResource("/images/tdiagram.gif"); //obavezno napraviti nove
+        }
+        else if(((MapTreeItem)val).getMapNode()instanceof Project){
+            imageURL = getClass().getResource("/images/tproject.gif");
+        }
+
+        Icon icon = null;
+        if(imageURL != null)
+            icon = new ImageIcon(imageURL);
+        setIcon(icon);
+
+
+        return this;
+    }
 }
