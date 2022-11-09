5746bcf78e20298bd77c339369178a9ae2a36eec
markocic <markokocic2000@gmail.com>
2022-11-09 16:38:38 +0100

    authorModal, AuthorAction; izbacuje modal za unos imena autora

--- src/main/java/raf/dsw/gerumap/gui/swing/controller/AuthorAction.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/controller/AuthorAction.java
@@ -1,5 +1,9 @@
 package raf.dsw.gerumap.gui.swing.controller;
 
+import raf.dsw.gerumap.gui.swing.view.AuthorModal;
+import raf.dsw.gerumap.gui.swing.view.InfoModal;
+import raf.dsw.gerumap.gui.swing.view.MainFrame;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
@@ -17,5 +21,7 @@ public class AuthorAction extends AbstractGerumapAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO implement dialog to take in author name and change it in project class
+        AuthorModal authorModal = new AuthorModal(MainFrame.getInstance());
+
     }
 }
---
+++ src/main/java/raf/dsw/gerumap/gui/swing/view/AuthorModal.java
@@ -0,0 +1,46 @@
+package raf.dsw.gerumap.gui.swing.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AuthorModal extends JDialog implements ActionListener {
+
+    public AuthorModal(JFrame parent) {
+        super(parent, "Author", true);
+        if (parent != null) {
+            Dimension parentSize = parent.getSize();
+            Point p = parent.getLocation();
+            setLocation(p.x + parentSize.width / 4, p.y + parentSize.height / 4);
+            setSize(parentSize.width / 4, parentSize.height / 4);
+        }
+        JPanel messagePane = new JPanel();
+        messagePane.add(new JLabel("Author: "));
+        JTextField authorField = new JTextField(12);
+        messagePane.add(authorField);
+        getContentPane().add(messagePane);
+        JPanel buttonPane = new JPanel();
+        JButton buttonSave = new JButton("Save");
+        JButton buttonCancel = new JButton("Cancel");
+        buttonPane.add(buttonSave);
+        buttonPane.add(buttonCancel);
+        buttonSave.addActionListener(this::saveActionPerformed);
+        buttonCancel.addActionListener(this);
+        getContentPane().add(buttonPane, BorderLayout.SOUTH);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // kod za cancel dugme, ne radi nista samo gasi modal
+        setVisible(false);
+        dispose();
+    }
+
+    public void saveActionPerformed(ActionEvent e) {
+        // kod za save dugme
+        System.out.println("save");
+    }
+}
