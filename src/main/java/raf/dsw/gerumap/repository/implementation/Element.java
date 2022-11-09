110fbb8f3f4cfe53e39e751cf22dcd93cc117274
markocic <markokocic2000@gmail.com>
2022-11-09 23:52:28 +0100

    Element klasa ne treba da ekstenduje mapnodecomposite, nema decu, shmrc :(

--- src/main/java/raf/dsw/gerumap/repository/implementation/Element.java
+++ src/main/java/raf/dsw/gerumap/repository/implementation/Element.java
@@ -3,19 +3,9 @@ package raf.dsw.gerumap.repository.implementation;
 import raf.dsw.gerumap.repository.composite.MapNode;
 import raf.dsw.gerumap.repository.composite.MapNodeComposite;
 
-public class Element extends MapNodeComposite {
+public class Element extends MapNode {
     public Element(String name, MapNode parent) {
         super(name, parent);
-    }
-
-    @Override
-    public void addChild(MapNode child) {
-
-    }
-
-    @Override
-    public void deleteChild(MapNode child) {
-
     }
 
     public Element() {
