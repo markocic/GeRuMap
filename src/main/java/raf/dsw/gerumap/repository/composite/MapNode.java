20b51114c658ae8771030c0d06b38a9ac21f2c23
Marko Kocic <markokocic2000@gmail.com>
2022-11-13 21:38:09 +0100

    implementirane metode za observer

--- src/main/java/raf/dsw/gerumap/repository/composite/MapNode.java
+++ src/main/java/raf/dsw/gerumap/repository/composite/MapNode.java
@@ -1,9 +1,17 @@
 package raf.dsw.gerumap.repository.composite;
 
-public abstract class MapNode {
+import raf.dsw.gerumap.core.IPublisher;
+import raf.dsw.gerumap.core.ISubscriber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class MapNode implements IPublisher {
 
     private String name;
     private MapNode parent;
+
+    private List<ISubscriber> subscribers;
 
     public MapNode(String name, MapNode parent) {
         this.name = name;
@@ -20,6 +28,27 @@ public abstract class MapNode {
         return false;
     }
 
+    @Override
+    public void addSubscriber(ISubscriber sub) {
+        if (sub == null) return;
+        if (this.subscribers == null) this.subscribers = new ArrayList<>();
+        if (this.subscribers.contains(sub)) return;
+        this.subscribers.add(sub);
+    }
+
+    @Override
+    public void removeSubscriber(ISubscriber sub) {
+        if(sub == null || this.subscribers == null || !this.subscribers.contains(sub)) return;
+        this.subscribers.remove(sub);
+    }
+
+    @Override
+    public void notifySubscribers(Object notification) {
+        for (ISubscriber sub : this.subscribers) {
+            sub.update(notification);
+        }
+    }
+
     public String getName() {
         return name;
     }
@@ -34,5 +63,13 @@ public abstract class MapNode {
 
     public void setParent(MapNode parent) {
         this.parent = parent;
+    }
+
+    public List<ISubscriber> getSubscribers() {
+        return subscribers;
+    }
+
+    public void setSubscribers(List<ISubscriber> subscribers) {
+        this.subscribers = subscribers;
     }
 }
