8b55f7c5268c6fc26e1677934e72f5cf129904ff
markocic <markokocic2000@gmail.com>
2022-11-16 16:19:01 +0100

    msggen u appcore

--- src/main/java/raf/dsw/gerumap/AppCore.java
+++ src/main/java/raf/dsw/gerumap/AppCore.java
@@ -3,12 +3,14 @@ package raf.dsw.gerumap;
 import raf.dsw.gerumap.core.ApplicationFramework;
 import raf.dsw.gerumap.core.Gui;
 import raf.dsw.gerumap.gui.swing.SwingGui;
+import raf.dsw.gerumap.logger.MessageGenerator;
 import raf.dsw.gerumap.repository.MapRepository;
 
 
 public class AppCore extends ApplicationFramework {
 
     private static AppCore instance;
+    private MessageGenerator msgGenerator = new MessageGenerator();
 
     private AppCore() {
     }
@@ -33,5 +35,13 @@ public class AppCore extends ApplicationFramework {
         appCore.initialise(gui, mapRepository);
         appCore.run();
 
+    }
+
+    public MessageGenerator getMsgGenerator() {
+        return msgGenerator;
+    }
+
+    public void setMsgGenerator(MessageGenerator msgGenerator) {
+        this.msgGenerator = msgGenerator;
     }
 }
