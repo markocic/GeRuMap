e196292e554c057463ca4de270b92e059d46b869
markocic <markokocic2000@gmail.com>
2022-11-16 15:46:50 +0100

    msg generator

--- src/main/java/raf/dsw/gerumap/gui/swing/observer/MsgPublisher.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/observer/MsgPublisher.java
@@ -1,8 +1,8 @@
 package raf.dsw.gerumap.gui.swing.observer;
 
 public interface MsgPublisher {
-    void addSubscriber(ISubscriber sub);
-    void removeSubscriber(ISubscriber sub);
+    void addSubscriber(MsgSubscriber sub);
+    void removeSubscriber(MsgSubscriber sub);
     void notifySubscribers(Object notification);
 
 }
--- src/main/java/raf/dsw/gerumap/logger/MessageGenerator.java
+++ src/main/java/raf/dsw/gerumap/logger/MessageGenerator.java
@@ -2,8 +2,19 @@ package raf.dsw.gerumap.logger;
 
 import raf.dsw.gerumap.gui.swing.observer.ISubscriber;
 import raf.dsw.gerumap.gui.swing.observer.MsgPublisher;
+import raf.dsw.gerumap.gui.swing.observer.MsgSubscriber;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessageGenerator implements MsgPublisher {
+
+    private String message;
+    private TipPoruke tipPoruke;
+    private Time timeStamp;
+
+    private List<MsgSubscriber> msgSubscriberList;
     public MessageGenerator() {
     }
 
@@ -16,12 +27,15 @@ public class MessageGenerator implements MsgPublisher {
 
 
     @Override
-    public void addSubscriber(ISubscriber sub) {
+    public void addSubscriber(MsgSubscriber sub) {
+        if (sub == null) return;
+        if (msgSubscriberList == null) msgSubscriberList = new ArrayList<>();
 
+        msgSubscriberList.add(sub);
     }
 
     @Override
-    public void removeSubscriber(ISubscriber sub) {
+    public void removeSubscriber(MsgSubscriber sub) {
 
     }
 
