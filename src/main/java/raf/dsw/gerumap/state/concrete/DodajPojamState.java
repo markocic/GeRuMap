b41d689c2b7eba5afefdb5da7daf32659d61b57b
markocic <markokocic2000@gmail.com>
2022-12-06 17:02:26 +0100

    poboljsan error check za dodajpojamstate

--- src/main/java/raf/dsw/gerumap/state/concrete/DodajPojamState.java
+++ src/main/java/raf/dsw/gerumap/state/concrete/DodajPojamState.java
@@ -1,11 +1,13 @@
 package raf.dsw.gerumap.state.concrete;
 
+import raf.dsw.gerumap.AppCore;
 import raf.dsw.gerumap.gui.swing.grafika.model.PojamModel;
 import raf.dsw.gerumap.gui.swing.grafika.painter.PojamPainter;
 import raf.dsw.gerumap.gui.swing.tree.MapTree;
 import raf.dsw.gerumap.gui.swing.tree.model.MapTreeItem;
 import raf.dsw.gerumap.gui.swing.view.MainFrame;
 import raf.dsw.gerumap.gui.swing.view.MapView;
+import raf.dsw.gerumap.logger.TipPoruke;
 import raf.dsw.gerumap.repository.implementation.MindMap;
 import raf.dsw.gerumap.state.State;
 
@@ -21,23 +23,16 @@ public class DodajPojamState extends State {
     public void mousePressedState(int x, int y, MapView map) {
         // ne dozvoljava korisniku da napravi "blank" pojam
         String pojamName = "";
+        boolean check = true;
         do {
             pojamName = JOptionPane.showInputDialog("Unesi ime novog pojma: ","Novi pojam");
+            if (pojamName.isBlank()) {
+                AppCore.getInstance().getMsgGenerator().generateMsg("Ime ne moze da bude prazno", TipPoruke.GRESKA);
+            }
+            else check = false;
 
-        } while (pojamName.isBlank());
+        } while (check);
 
-        // izgleda da ne moraju elementi da se reflektuju u JTree tako da sam dosta vremena bacio u vodu al bar necu morati jos koji sat :)
-//        // pravljenje novog objekta instance Element i povezivanje sa mapom
-//        Enumeration<TreeNode> children = ((MapTree)MainFrame.getInstance().getMapTree()).getOpenedNode().children();
-//        MapTreeItem childMap = null;
-//        while(children.hasMoreElements()) {
-//            childMap = (MapTreeItem) children.nextElement();
-//            if (childMap.getMapNode().getName().equals(map.getMapa().getName())) {
-//                // nasli smo mapu, dodajemo joj dete
-//                // TODO implementirati proveru da li odabrano ime vec postoji kao element u mapi
-//                MainFrame.getInstance().getMapTree().addChild(childMap, pojamName);
-//            }
-//        }
         PojamModel pojamModel = new PojamModel(pojamName, new Point(x, y), new Dimension(80, 40), 2, Color.BLACK);
         PojamPainter pojamPainter = new PojamPainter(pojamModel);
         map.addPainter(pojamPainter);
