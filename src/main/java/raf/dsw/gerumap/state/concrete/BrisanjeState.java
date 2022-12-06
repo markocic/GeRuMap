904827f9d8be9a248b369f3c7c759fba4e3574e9
markocic <markokocic2000@gmail.com>
2022-12-06 16:55:57 +0100

    provere gresaka za brisanjeState odradjene

--- src/main/java/raf/dsw/gerumap/state/concrete/BrisanjeState.java
+++ src/main/java/raf/dsw/gerumap/state/concrete/BrisanjeState.java
@@ -1,32 +1,34 @@
 package raf.dsw.gerumap.state.concrete;
 
-import raf.dsw.gerumap.gui.swing.grafika.model.PojamModel;
+import raf.dsw.gerumap.AppCore;
 import raf.dsw.gerumap.gui.swing.grafika.model.VezaModel;
 import raf.dsw.gerumap.gui.swing.grafika.painter.ElementPainter;
 import raf.dsw.gerumap.gui.swing.grafika.painter.PojamPainter;
 import raf.dsw.gerumap.gui.swing.grafika.painter.VezaPainter;
-import raf.dsw.gerumap.gui.swing.tree.MapTree;
-import raf.dsw.gerumap.gui.swing.tree.model.MapTreeItem;
-import raf.dsw.gerumap.gui.swing.view.MainFrame;
 import raf.dsw.gerumap.gui.swing.view.MapView;
-import raf.dsw.gerumap.repository.implementation.MindMap;
+import raf.dsw.gerumap.logger.TipPoruke;
 import raf.dsw.gerumap.state.State;
 
-import javax.swing.tree.TreeNode;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Enumeration;
 
 public class BrisanjeState extends State {
     @Override
     public void mousePressedState(int x, int y, MapView map) {
         ElementPainter selected = getPainterAtClickedLocation(new Point(x, y), map);
-        if (!(selected instanceof PojamPainter)) return;
+        if (!(selected instanceof PojamPainter)) {
+            AppCore.getInstance().getMsgGenerator().generateMsg("Kliknite na selektovani pojam koji zelite da obrisete", TipPoruke.OBAVJESTENJE);
+            return;
+        }
 
         // brisanje pojma sa radne povrsine
         if (map.getSelectedPainters().contains(selected)) map.deleteSelectedPainters();
-        else return;
+        else {
+            AppCore.getInstance().getMsgGenerator().generateMsg("Pojam na koji ste kliknuli nije selektovan", TipPoruke.OBAVJESTENJE);
+            return;
+        }
 
+        // brisanje veza koje su povezane sa pojmom koji brisemo
         ArrayList<ElementPainter> vezaPainterList = new ArrayList<>();
         for (ElementPainter painter : map.getPainters()) {
             if (painter instanceof VezaPainter) {
@@ -38,30 +40,6 @@ public class BrisanjeState extends State {
         }
 
         map.removeAllPainters(vezaPainterList);
-
-
-//        // brisanje pojma iz JTree
-//        MapTreeItem mindMapTreeItem = getMindMapTreeItem(map.getMapa());
-//        Enumeration<TreeNode> elements = mindMapTreeItem.children();
-//        MapTreeItem element;
-//        while (elements.hasMoreElements()) {
-//            element = (MapTreeItem) elements.nextElement();
-//            if (element.getMapNode().getName().equals( ((PojamModel) selected.getElement()).getName())) {
-//                MainFrame.getInstance().getMapTree().deleteChild(element);
-//            }
-//        }
     }
 
-    public MapTreeItem getMindMapTreeItem(MindMap map) {
-        Enumeration<TreeNode> children = ((MapTree)MainFrame.getInstance().getMapTree()).getOpenedNode().children();
-        MapTreeItem childMap = null;
-        while(children.hasMoreElements()) {
-            childMap = (MapTreeItem) children.nextElement();
-            if (childMap.getMapNode().getName().equals(map.getName())) {
-                return childMap;
-            }
-        }
-
-        return null;
-    }
 }
