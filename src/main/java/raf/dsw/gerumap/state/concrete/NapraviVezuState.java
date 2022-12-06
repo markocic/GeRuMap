0c86979e36c6b6dd840371929755033029ac03b6
markocic <markokocic2000@gmail.com>
2022-12-06 01:31:41 +0100

    par smernica kako treba da radi state za pravljenje veze

--- src/main/java/raf/dsw/gerumap/state/concrete/NapraviVezuState.java
+++ src/main/java/raf/dsw/gerumap/state/concrete/NapraviVezuState.java
@@ -6,15 +6,23 @@ import raf.dsw.gerumap.gui.swing.view.MapView;
 import raf.dsw.gerumap.state.State;
 
 public class NapraviVezuState extends State {
-    PojamModel pojamModelOd;
-    PojamModel pojamModelDo;
+
     @Override
     public void mousePressedState(int x, int y, MapView map) {
+        // ovde postaviti prvi point za liniju
+        // objekat na x i y koordinati predstavlja od pojam
+
 
     }
 
     @Override
     public void mouseReleasedState(int x, int y, MapView map) {
+        // na samom kraju, ove x i y koordinate postaviti kao krajnju tacku
+        // uzeti objekat na x i y koordinati i to ce biti do pojam
+    }
 
+    @Override
+    public void mouseDraggedState(int x, int y, MapView map) {
+        // za svaki dogadjaj uraditi update da trenutno mesto misa bude drugi point linije
     }
 }
