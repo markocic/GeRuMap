939605439c5d27db294b5efdf56e70ce9d3c9ac5
markocic <markokocic2000@gmail.com>
2022-12-09 14:47:58 +0100

    bagovi za podesavanja popravljeni, dozvoljeno menjanje samo pojma, samo veze ili vise elemenata

--- src/main/java/raf/dsw/gerumap/state/concrete/PodesavanjaState.java
+++ src/main/java/raf/dsw/gerumap/state/concrete/PodesavanjaState.java
@@ -4,6 +4,7 @@ import raf.dsw.gerumap.AppCore;
 import raf.dsw.gerumap.gui.swing.grafika.model.PojamModel;
 import raf.dsw.gerumap.gui.swing.grafika.painter.ElementPainter;
 import raf.dsw.gerumap.gui.swing.grafika.painter.PojamPainter;
+import raf.dsw.gerumap.gui.swing.grafika.painter.VezaPainter;
 import raf.dsw.gerumap.gui.swing.view.MainFrame;
 import raf.dsw.gerumap.gui.swing.view.MapView;
 import raf.dsw.gerumap.gui.swing.view.PodesavanjaModal;
@@ -15,21 +16,26 @@ import java.awt.*;
 public class PodesavanjaState extends State {
     @Override
     public void mousePressedState(int x, int y, MapView map) {
-        // TODO: postoji bag da ne primeni podesavanja ukoliko se klikne samo na vezu bez da se pre toga selektuje ista
-        ElementPainter selected = getPainterAtClickedLocation(new Point(x, y), map);
+        if (map.getSelectedPainters().isEmpty()) {
+            AppCore.getInstance().getMsgGenerator().generateMsg("Morate selektovati elemente koje zelite da menjate", TipPoruke.OBAVJESTENJE);
+            return;
+        }
 
-        boolean imePrompt = map.getSelectedPainters().size() < 2 && selected instanceof PojamPainter;
-        System.out.println(map.getSelectedPainters().size());
+        // proveravamo kakva vrsta promena je u pitanju
+        boolean singlePojam = map.getSelectedPainters().size() == 1 && map.getSelectedPainters().get(0) instanceof PojamPainter;
+        boolean singleVeza = map.getSelectedPainters().size() == 1 && map.getSelectedPainters().get(0) instanceof VezaPainter;
+        boolean multiChange = map.getSelectedPainters().size() > 1;
 
-        String name = (imePrompt) ? ((PojamModel) selected.getElement()).getName() : null;
+        ElementPainter selected = map.getSelectedPainters().get(0);
+        String name = (singlePojam) ? ((PojamModel) selected.getElement()).getName() : null;
         Color color = selected.getElement().getColor();
         int stroke = selected.getElement().getStroke();
 
 
-        PodesavanjaModal modal = new PodesavanjaModal(MainFrame.getInstance(), name, color, stroke, imePrompt);
+        PodesavanjaModal modal = new PodesavanjaModal(MainFrame.getInstance(), name, color, stroke, singlePojam);
 
         // dalja validacija imena, takodje se preskace ako je selektovano vise elemenata
-        if (imePrompt) {
+        if (singlePojam) {
             while (modal.getName() != null && pojamNameExists(modal.getName(), map) && !modal.getName().equals(name)) {
                 AppCore.getInstance().getMsgGenerator().generateMsg("Pojam sa imenom '" + modal.getName() +"' vec postoji", TipPoruke.GRESKA);
                 modal.setVisible(true);
@@ -39,8 +45,9 @@ public class PodesavanjaState extends State {
 
         if (!modal.isConfirmed()) return;
 
-        if (!imePrompt) multiSelectChange(map, modal.getColor(), modal.getStroke());
-        else singleSelectChange(selected, modal.getName(), modal.getColor(), modal.getStroke());
+        if (multiChange) multiSelectChange(map, modal.getColor(), modal.getStroke());
+        else if (singlePojam) singlePojamChange(selected, modal.getName(), modal.getColor(), modal.getStroke());
+        else singleVezaChange(selected, modal.getColor(), modal.getStroke());
 
     }
 
@@ -51,9 +58,14 @@ public class PodesavanjaState extends State {
         }
     }
 
-    public void singleSelectChange(ElementPainter painter, String name, Color color, int stroke) {
+    public void singlePojamChange(ElementPainter painter, String name, Color color, int stroke) {
         painter.getElement().setStroke(stroke);
         painter.getElement().setColor(color);
         ((PojamModel) painter.getElement()).setName(name);
+    }
+
+    public void singleVezaChange(ElementPainter painter, Color color, int stroke) {
+        painter.getElement().setStroke(stroke);
+        painter.getElement().setColor(color);
     }
 }
