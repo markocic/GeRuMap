bec25a046726acc9d91e0919703aa8ad77b899e8
Danilo149 <danilo.g.joncic2@gmail.com>
2022-12-21 16:11:23 +0100

    Command za akcije dodate,prazne treba naci nacin da se unutar njih doda ono sto je bitno

---
+++ src/main/java/raf/dsw/gerumap/gui/swing/command/concreteCommands/VezaCommand.java
@@ -0,0 +1,15 @@
+package raf.dsw.gerumap.gui.swing.command.concreteCommands;
+
+import raf.dsw.gerumap.gui.swing.command.AbstractCommand;
+
+public class VezaCommand extends AbstractCommand {
+    @Override
+    public void doCommand() {
+
+    }
+
+    @Override
+    public void undoCommand() {
+
+    }
+}
